bc138eb1c9bb8ff1869203f0438f863d4dccd853
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-02-08 22:02:23 +0530

    Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div  className='w-full h-screen bg-white'>
       <Navbar />
+      <LandingPage />
     </div>
   )
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen '>
+        <div className='textstructure'>
+            <div className='masker'>
+                <h1 className='uppercase text-7xl font-["Founders_Grotesk"]'>We Create</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
