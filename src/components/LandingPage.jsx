7cfdc078b74b1dfd30645b246d6abb06ee193292
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-02-09 23:10:59 +0530

    Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,17 +7,24 @@ function LandingPage() {
         {["We Create", "Eye Opening", "Presentations"].map((item) => {
           return (
             <>
-            <div className="masker">
-              <h1 className='uppercase text-[7vw] leading-[6vw] text-wrap tracking-tighter font-semibold leading-none font-["Founders_Grotesk"] '>
-                {item}
-              </h1>
-            </div>
+              <div className="masker">
+                <h1 className='uppercase text-[7vw] leading-[6vw] text-wrap tracking-tighter font-semibold leading-none font-["Founders_Grotesk"] '>
+                  {item}
+                </h1>
+              </div>
             </>
           );
         })}
       </div>
-      <div className="border-t-2 border-zinc-400 mt-28">
-
+      <div className="border-t-2 border-zinc-400 mt-28 flex justify-between items-center py-5 px-20">
+        {[
+          "For Public and private Companies",
+          "From the first pitch to IPO",
+        ].map((item, index) => (
+          <p className="text-md font-light tracking-tight leading-none">
+            {item}
+          </p>
+        ))}
       </div>
     </div>
   );
