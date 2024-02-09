5f622179b2cfd00c926fd9e962cbabb174170084
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-02-09 22:00:48 +0530

    Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen pt-1'>
-        <div className='textstructure mt-40 px-8'>
-            <div className='masker'>
-                <h1 className='uppercase text-8xl text-wrap tracking-tighter font-semibold leading-none font-["Founders_Grotesk"] '>We Create</h1>
+    <div className="w-full h-screen pt-1">
+      <div className="textstructure mt-40 px-8">
+        {["We Create", "Eye Opening", "Presentations"].map((item) => {
+          return (
+            <>
+            <div className="masker">
+              <h1 className='uppercase text-[7vw] leading-[6vw] text-wrap tracking-tighter font-semibold leading-none font-["Founders_Grotesk"] '>
+                {item}
+              </h1>
             </div>
-        </div>
+            </>
+          );
+        })}
+      </div>
+      <div className="border-t-2 border-zinc-400 mt-28">
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
