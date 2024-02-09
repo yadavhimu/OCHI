b0d061e6a2d8455fb4c7f563593e8e98b65f17ee
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2024-02-09 19:00:01 +0530

    Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function LandingPage() {
   return (
     <div className='w-full h-screen pt-1'>
-        <div className='textstructure mt-40 px-20'>
+        <div className='textstructure mt-40 px-8'>
             <div className='masker'>
-                <h1 className='uppercase text-7xl leading-none font-["Founders_Grotesk"]'>We Create</h1>
+                <h1 className='uppercase text-8xl text-wrap tracking-tighter font-semibold leading-none font-["Founders_Grotesk"] '>We Create</h1>
             </div>
         </div>
     </div>
